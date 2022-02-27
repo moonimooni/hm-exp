d4b89090cde328bae617fb30b0c17d3d5f6c5fac
moonimooni <heewonmoon@day1company.co.kr>
2022-02-27 19:07:26 +0900

    fix: constant 재할당 코드 삭제

--- config/index.js
+++ config/index.js
@@ -2,9 +2,9 @@ require('dotenv').config();
 
 const { merge } = require('lodash');
 const env = process.env.NODE_ENV ?? 'local';
-const config = { environment: env, ...require('./default') };
 const isLocal = env === 'local' || env === 'test';
 
+let config = { environment: env, ...require('./default') };
 if (!isLocal) {
   config = merge(config, require(`./${env}`));
 }
