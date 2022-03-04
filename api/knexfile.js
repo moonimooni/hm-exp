4fd2c71b179943a5f2cf6c6d303c9027103b5d38
moonimooni <heewonmoon@day1company.co.kr>
2022-03-04 13:00:54 +0900

    refactor: knexfile 단순화

--- api/knexfile.js
+++ api/knexfile.js
@@ -1,15 +1,2 @@
-// Update with your config settings.
-
-/**
- * @type { Object.<string, import("knex").Knex.Config> }
- */
-
 const { mysql } = require('../config');
-
-module.exports = {
-  local: mysql,
-
-  development: mysql,
-
-  production: mysql,
-};
+module.exports = mysql;
