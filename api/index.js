b823910a629456cae99be5f0dcc20250dfabecfd
byeong jae lee <dbj1007@cambee.co.kr>
2022-03-07 11:31:07 +0900

    modify: /api index.js migrate code 삭제

--- api/index.js
+++ api/index.js
@@ -5,9 +5,6 @@ const { router } = require('./routes');
 
 const app = express();
 
-const knex = require('knex')(config.mysql);
-knex.migrate.latest();
-
 app.use(express.json());
 app.use(cors());
 app.use('/api', router);
