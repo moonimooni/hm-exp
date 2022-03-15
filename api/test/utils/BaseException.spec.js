b16c3ead4133274d55802cd6340e572f18493565
byeong jae lee <redgpcmffld@gmail.com>
2022-03-15 10:22:53 +0900

    feat: BaseException Class 및 test code 작성

--- api/test/utils/BaseException.spec.js
+++ api/test/utils/BaseException.spec.js
@@ -1,4 +1,22 @@
-// ADD LATER
+const { BaseException } = require('../../utils/BaseException');
+
 describe('BaseException', () => {
-  test.skip('', () => {});
+  it('should be throw Error with code and message of the entered parameters.', () => {
+    const code = 400;
+    const message = 'test message';
+    try {
+      throw new BaseException({ code, message });
+    } catch (e) {
+      expect(e.code).toBe(code);
+      expect(e.message).toBe(message);
+    }
+  });
+  it("should throw Error with code 500 and message 'unknown error' when no parameters were passed", () => {
+    try {
+      throw new BaseException();
+    } catch (e) {
+      expect(e.code).toBe(500);
+      expect(e.message).toBe('unknown error');
+    }
+  });
 });
--- api/utils/BaseException.js
+++ api/utils/BaseException.js
@@ -1,7 +1,18 @@
 //TODO: namespace?
 
+const { isNumber, isString } = require('lodash');
+
 class BaseException extends Error {
-  constructor() {}
+  /**
+   * @param {object} error_info Error code and message
+   * @param {number} error_info.code Error code
+   * @param {string} error_info.message Error message
+   */
+  constructor({ code = 500, message = 'unknown error' } = {}) {
+    super(message);
+    this.code = code;
+    this.name = this.constructor.name;
+  }
 }
 
 class BadRequestException extends BaseException {
